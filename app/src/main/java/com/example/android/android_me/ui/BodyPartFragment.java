86292c9d0b6f63013b6618bd1a13c5af2d95562e
Anahi Costa <moanableu@gmail.com>
2018-11-10 18:54:16 -0500

    gradle files in synch

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,32 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.R;
+
+public class BodyPartFragment extends Fragment {
+
+    public BodyPartFragment(){
+        //required to instantiate this fragment
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+
+        // reference to obj in layout
+        ImageView imageView = rootView.findViewById(R.id.body_part_image_view);
+
+        imageView.setImageResource(AndroidImageAssets.getHeads().get(0)); // missing assets in starter code repo
+
+        return rootView;
+    }
+}
